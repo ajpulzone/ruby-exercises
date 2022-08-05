f2c77dfec0d8387754c2d79ae63c60ec515b934c
Annie Pulzone <ajpulzone@gmail.com>
2022-08-04 17:34:52 -0700

    Complete person exercise

---
+++ command-query/exercises/person.rb
@@ -0,0 +1,18 @@
+class Person
+  attr_reader :age, :bday
+
+  def initialize
+    @age = 0
+    @bday
+  end
+
+  def age
+    @age
+  end
+
+  def happy_birthday
+    @bday
+      if @bday = (@age += 1)
+      end
+    end
+  end
