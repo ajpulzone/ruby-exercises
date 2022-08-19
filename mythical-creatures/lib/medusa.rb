3abdf2a1dc0074c800467210e6f5fee722daa837
Annie Pulzone <ajpulzone@gmail.com>
2022-08-19 10:29:04 -0700

    Complete turns to stone exercise

---
+++ mythical-creatures/lib/medusa.rb
@@ -0,0 +1,31 @@
+class Medusa
+attr_reader :name
+
+  def initialize(name)
+      @name = name
+      @statues = []
+  end
+
+  def statues
+    return @statues
+  end
+
+  def stare(victim)
+    @statues << victim
+    victim.stoned = true
+  end
+end
+
+class Person
+  attr_reader :name
+  attr_writer :stoned
+
+  def initialize(name)
+    @name = name
+    @stoned = false
+  end
+
+  def stoned?
+    @stoned
+    end
+  end
