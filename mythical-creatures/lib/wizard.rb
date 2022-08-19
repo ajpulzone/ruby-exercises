d62d1de2cf0901ac0abe002554aa096ff6beb9af
Annie Pulzone <ajpulzone@gmail.com>
2022-08-18 20:47:57 -0700

    Complete wizard lib

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -4,8 +4,12 @@ class Wizard
   def initialize(name, bearded={bearded: true})
     @name = name
     @bearded = bearded
+    @casts = 0
   end
 
+# I don't understand how lines 13-16 work, copied from rcm3200 github. Need to understand how
+# @bearded[:bearded] == false works, also the same goes for line 4 bearded argument. How/why does that work?
+# What is being done on line 4?
   def bearded?
     if @bearded[:bearded] == false
        @bearded = false
@@ -15,5 +19,23 @@ class Wizard
 
    def incantation(message)
      "sudo #{message}"
-   end  
+   end
+
+   def rested?
+     if @casts < 3
+       true
+     else
+       false
+     end
+   end
+
+#I don't understant lines 34-36, copied from rcm32000 git hub. I wanted to be able to have cast do 2 things
+# within the method definition: 1.) Have the message "MAGIC MISSILE" and keep count for each time cast
+# is called to wizard so the wizzard will no longer be rested. What does the cast(*) do? Why does that allow
+# 2 things to happen within the code block
+# Why does spell not need an @ symbol?
+   def cast(*)
+     @casts += 1
+     spell = "MAGIC MISSILE"
+   end
 end
