e16eeb19e1a75834f83bc503b18e8fba66de5221
Annie Pulzone <ajpulzone@gmail.com>
2022-08-20 20:53:53 -0700

    Clean up wizard cast code block

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -29,13 +29,8 @@ class Wizard
      end
    end
 
-#I don't understant lines 34-36, copied from rcm32000 git hub. I wanted to be able to have cast do 2 things
-# within the method definition: 1.) Have the message "MAGIC MISSILE" and keep count for each time cast
-# is called to wizard so the wizzard will no longer be rested. What does the cast(*) do? Why does that allow
-# 2 things to happen within the code block
-# Why does spell not need an @ symbol?
-   def cast(*)
+   def cast
      @casts += 1
-     spell = "MAGIC MISSILE"
+     "MAGIC MISSILE"
    end
 end
